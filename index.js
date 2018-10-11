9c70e392604756c02b4103335b19217a800041b7
Swizec Teller <swizec@swizec.com>
2018-10-11 00:10:43 -0700

    Average last 3 measurements before scaling workload

--- index.js
+++ index.js
@@ -3,28 +3,37 @@ const mri = require("mri");
 const sll = require("single-line-log").stdout;
 const kleur = require("kleur");
 const fork = require("child_process").fork;
+const sleep = require("sleep");
 
 async function readTemp() {
     const temp = await si.cpuTemperature();
     return { main: temp.main, cores: temp.cores };
 }
 
+function avg(arr) {
+    return arr.reduce((sum, t) => t.main + sum, 0) / arr.length;
+}
+
 async function thawMyCarrots(targetTemp) {
     let temp = await readTemp(),
+        temps = [temp, temp, temp],
         children = [];
 
     while (1) {
-        if (temp.main < targetTemp) {
+        if (avg(temps) < targetTemp) {
             if (children.length < 10) {
                 children.push(fork("./eat-cpu"));
             }
-        } else if (temp.main > targetTemp) {
+        } else if (avg(temps) > targetTemp) {
             if (children.length > 0) {
                 children.pop().kill();
             }
         }
 
         temp = await readTemp();
+        temps.shift();
+        temps.push(temp);
+
         sll(
             "Current temp",
             kleur.bold.red(temp.main.toFixed(2)),
@@ -32,6 +41,7 @@ async function thawMyCarrots(targetTemp) {
             kleur.bold.gray(temp.cores),
             `\nChildren: ${children.length}`
         );
+        sleep.msleep(500);
     }
 }
 
